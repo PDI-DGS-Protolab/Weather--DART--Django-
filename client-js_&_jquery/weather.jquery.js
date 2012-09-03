9ece2f0d3d26551fe3119350260c592f2c9b740a
Alberto Blázquez <hola@albertoblazquez.net>
2012-09-03 20:09:17 +0300

    Update client-js_&_jquery/weather.jquery.js
    Fixed some errors because the code was not completely implemented with jQuery

--- client-js_&_jquery/weather.jquery.js
+++ client-js_&_jquery/weather.jquery.js
@@ -1,125 +1,135 @@
-
 	function login_fun() {
-    	var json = {
-    	    user : $("user_id").text(),
-    	    pass : $("pass_id").text()
-    	};
+	
+    		var json = {
+    	    		user : $('#user_id').text(),
+    	    		pass : $('#pass_id').text()
+    		};
   		
   		$.post("http://127.0.0.1:8000/weather/login", json)
-  		    .success(function() {
-					$("log_form").style.display = 'none';
-					$("logged_user").style.display = 'block';
-					$("logged_user").textContent = $("user_id").value;
-					$("logout_button").style.display = 'block';
-					$("error").style.display = 'none';
-  		    }).failure(function() {
-		 			$('error_text').textContent = "The user and/or the password are not correct";
-		 			$('error').style.display="";
-  		    });
-  	}
+  			.success(function() {
+				$('#log_form').style.display = 'none';
+				$('#logged_user').style.display = 'block';
+				$('#logged_user').text( user );
+				$('#logout_button').style.display = 'block';
+				$('#error').style.display = 'none';
+			
+  			}).error(function() {
+		 		$('#error_text').text("The user and/or the password are not correct");
+		 		$('#error').style.display= '';
+		 	
+			});
+
+	}
+
 
 	function logout_fun() {
-    	$.post("http://127.0.0.1:8000/weather/logout", {}, function() {
-    		  $('log_form').style.display = '';
-	          $('logged_user').style.display = 'none';
-	          $('logout_button').style.display = 'none';
-	          $('content').style.display = 'none';
-	          $('error').style.display = 'none';
-    	});
+		
+    		$.post("http://127.0.0.1:8000/weather/logout", {}, function() {
+    			$('#log_form').style.display = '';
+	        	$('#logged_user').style.display = 'none';
+	        	$('#logout_button').style.display = 'none';
+	        	$('#content').style.display = 'none';
+	        	$('#error').style.display = 'none';
+    		});
 	
-	  // Poner equivalente a : req_logout.withCredentials = true;
+	  	// Poner equivalente a : req_logout.withCredentials = true;
 	  
 	}
 
+
 	function search_fun() {
 
-		var city = $("city").value;
+		var city = $('#city').text();
 		  
 		// Create and open a new request
-		var req_search = new XMLHttpRequest();
-		req_search.open("GET", "http://127.0.0.1:8000/weather/" + city +"/", true);
-		req_search.withCredentials = true;
-		req_search.onreadystatechange = function() {
-			// Add an event handler to call the onSuccess function when the POST call is successful
-			if (req_search.readyState == XMLHttpRequest.DONE) {
-				if (req_search.status == 200) {
-					$('error').style.display = 'none';
-					$('content').style.display = '';
-			 		onSuccess(req_search.responseText);
-			     	} else if (req_search.status == 404) {
-					$('error_text').textContent = "City " + city + " not found";
-			 		$('error').style.display = '';
-			     	} else if (req_search.status == 403) {
-			 		$('error_text').textContent = "You are not logged in";
-			 		$('error').style.display = '';
-			 	}
+		var URL = "http://127.0.0.1:8000/weather/" + city +"/";
+		$.get(URL).success(function(data) {
+			$('#error').style.display = 'none';
+			$('#content').style.display = '';
+			onSuccess(data);
+			
+		}).error(function(res) {
+			if (res.status == 404) {
+				$('#error_text').text("City " + city + " not found");
+				$('#error').style.display = '';
+			} else if (res.status == 403) {
+				$('#error_text').text("You are not logged in");
+			 	$('#error').style.display = '';
 			}
-		};
-
-		// Set the content type of the call and send the data
-		req_search.send();
-
-	}
+		});
+		
+	} 
+	
 
 	function onSuccess(json) {
 		var parsedJSON = JSON.parse(json);
 
-		$('city_name').textContent = parsedJSON["data"]["request"][0]["query"];
+		$('#city_name').text( parsedJSON["data"]["request"][0]["query"] );
 		  
 		// Current condition
 		var currentConditions = parsedJSON["data"]["current_condition"][0];
 		
-		$('observationTime').text(currentConditions["observation_time"]);
+		$('#observationTime').text( currentConditions["observation_time"] );
 		
-		$('description1').textContent = currentConditions["weatherDesc"][0]["value"];
-		$('tCelsius').textContent = currentConditions["temp_C"] + 'ºC';
-		$('tFarenheit').textContent = currentConditions["temp_F"]+ 'ºF';
-		$('humidity').textContent = currentConditions["humidity"]+ '%';
-		$('precipitation1').textContent = currentConditions["precipMM"]+ ' MM';
-		$('cloudcover').textContent =  currentConditions["cloudcover"]+ '%';
-		$('visibility').textContent =  currentConditions["visibility"]+ 'Km';
-		$('pressure').textContent =  currentConditions["pressure"];
-		$('windDirectionDegree1').textContent =  currentConditions["winddirDegree"]+ 'º';
-		$('windDirection16Point1').textContent =  currentConditions["winddir16Point"];
-		$('windSpeedkmh1').textContent =  currentConditions["windspeedKmph"]+ 'Kmph';
-		$('windSpeedMiles1').textContent =  currentConditions["windspeedMiles"]+ 'Mph';
-		$('img1').src = currentConditions["weatherIconUrl"][0]["value"];
+		$('#description1').text( currentConditions["weatherDesc"][0]["value"] );
+		$('#tCelsius').text( currentConditions["temp_C"] + 'ºC' );
+		$('#tFarenheit').text( currentConditions["temp_F"]+ 'ºF' );
+		$('#humidity').text( currentConditions["humidity"]+ '%' );
+		$('#precipitation1').text( currentConditions["precipMM"]+ ' MM' );
+		$('#cloudcover').text( currentConditions["cloudcover"]+ '%' );
+		$('#visibility').text( currentConditions["visibility"]+ 'Km' );
+		$('#pressure').text( currentConditions["pressure"] );
+		$('#windDirectionDegree1').text( currentConditions["winddirDegree"]+ 'º' );
+		$('#windDirection16Point1').text( currentConditions["winddir16Point"] );
+		$('#windSpeedkmh1').text( currentConditions["windspeedKmph"]+ 'Kmph' );
+		$('#windSpeedMiles1').text( currentConditions["windspeedMiles"]+ 'Mph' );
+		
+		$('#img1').src = currentConditions["weatherIconUrl"][0]["value"];
+		 
 		 
 		// Today's forecast
 		var weatherToday = parsedJSON["data"]["weather"][0];
-		$('dateToday').textContent = weatherToday["date"];
-		$('description2').textContent = weatherToday["weatherDesc"][0]["value"];
-		$('minTCelsius1').textContent = weatherToday["tempMinC"]+ 'ºC';
-		$('minTFarenheit1').textContent = weatherToday["tempMinF"]+ 'ºF';
-		$('maxTCelsius1').textContent = weatherToday["tempMaxC"]+ 'ºC';
-		$('maxTFarenheit1').textContent = weatherToday["tempMaxF"]+ 'ºF';
-		$('precipitation2').textContent = weatherToday["precipMM"]+ 'MM';
-		$('windDirectionDegree2').textContent =  weatherToday["winddirDegree"]+ 'º';
-		$('windDirection16Point2').textContent =  weatherToday["winddir16Point"];
-		$('windSpeedkmh2').textContent =  weatherToday["windspeedKmph"]+ 'Kmph';
-		$('windSpeedMiles2').textContent =  weatherToday["windspeedMiles"]+ 'Mph';
-		$('img2').src = weatherToday["weatherIconUrl"][0]["value"];
-		  
+
+		$('#dateToday').text( weatherToday["date"] );
+		$('#description2').text( weatherToday["weatherDesc"][0]["value"] );
+		$('#minTCelsius1').text( weatherToday["tempMinC"]+ 'ºC' ) );
+		$('#minTFarenheit1').text( weatherToday["tempMinF"]+ 'ºF' ) );
+		$('#maxTCelsius1').text( weatherToday["tempMaxC"]+ 'ºC' );
+		$('#maxTFarenheit1').text( weatherToday["tempMaxF"]+ 'ºF' );
+		$('#precipitation2').text( weatherToday["precipMM"]+ 'MM' );
+		$('#windDirectionDegree2').text( weatherToday["winddirDegree"]+ 'º' );
+		$('#windDirection16Point2').text( weatherToday["winddir16Point"] );
+		$('#windSpeedkmh2').text( weatherToday["windspeedKmph"]+ 'Kmph' );
+		$('#windSpeedMiles2').text( weatherToday["windspeedMiles"]+ 'Mph' );
+		
+		$('#img2').src = weatherToday["weatherIconUrl"][0]["value"];
+		
+
 		// Tomorrow's forecast
 		var weatherTomorrow = parsedJSON["data"]["weather"][1];
-		$('dateTomorrow').textContent = weatherTomorrow["date"];
-		$('description3').textContent = weatherTomorrow["weatherDesc"][0]["value"];
-		$('minTCelsius2').textContent = weatherTomorrow["tempMinC"]+ 'ºC';
-		$('minTFarenheit2').textContent = weatherTomorrow["tempMinF"]+ 'ºF';
-		$('maxTCelsius2').textContent = weatherTomorrow["tempMaxC"]+ 'ºC';
-		$('maxTFarenheit2').textContent = weatherTomorrow["tempMaxF"]+ 'ºF';
-		$('precipitation3').textContent = weatherTomorrow["precipMM"]+ 'MM';
-		$('windDirectionDegree3').textContent =  weatherTomorrow["winddirDegree"]+ 'º';
-		$('windDirection16Point3').textContent =  weatherTomorrow["winddir16Point"];
-		$('windSpeedkmh3').textContent =  weatherTomorrow["windspeedKmph"]+ 'Kmph';
-		$('windSpeedMiles3').textContent =  weatherTomorrow["windspeedMiles"]+ 'Mph';
-		$('img3').src = weatherTomorrow["weatherIconUrl"][0]["value"];
+
+		$('#dateTomorrow').text( weatherTomorrow["date"] );
+		$('#description3').text( weatherTomorrow["weatherDesc"][0]["value"] );
+		$('#minTCelsius2').text( weatherTomorrow["tempMinC"]+ 'ºC' );
+		$('#minTFarenheit2').text( weatherTomorrow["tempMinF"]+ 'ºF' );
+		$('#maxTCelsius2').text( weatherTomorrow["tempMaxC"]+ 'ºC' );
+		$('#maxTFarenheit2').text( weatherTomorrow["tempMaxF"]+ 'ºF' );
+		$('#precipitation3').text( weatherTomorrow["precipMM"]+ 'MM' );
+		$('#windDirectionDegree3').text( weatherTomorrow["winddirDegree"]+ 'º' );
+		$('#windDirection16Point3').text( weatherTomorrow["winddir16Point"] );
+		$('#windSpeedkmh3').text( weatherTomorrow["windspeedKmph"]+ 'Kmph' );
+		$('#windSpeedMiles3').text( weatherTomorrow["windspeedMiles"]+ 'Mph' );
+
+		$('#img3').src = weatherTomorrow["weatherIconUrl"][0]["value"];
 	}
 
+
 	function hide_error() {
-		$('error').style.display = 'none';
+		$('#error').style.display = 'none';
 	}
 	
-	$("formu_login").on('submit', login_fun);
-	$("formulary").on('submit', search_fun);
+	
+	// Event Handlers Assignment
+	$('#formu_login').on('submit', login_fun);
+	$('#formulary').on('submit', search_fun);
 
